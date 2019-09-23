0a2d044d756fe9356c73e2750cb50abaaadd644d
Olga Kripulevich <ochuvasheva@gmail.com>
2019-09-23 09:40:42 +0300

    Create PalindromeNumber.cs

---
+++ NET.Autumn.2019.Kripulevich.04/PalindromeNumber.cs
@@ -0,0 +1,44 @@
+﻿namespace Logic
+{
+    public class PalindromeNumber : IPredicate
+    {
+        public bool IsMatch(int number)
+        {
+            var digits = IntToArray(number);
+            return DoesThisArrayAPalindrome(digits, 0, digits.Length - 1);
+        }
+
+        public bool DoesThisArrayAPalindrome(int[] digits, int first, int last)
+        {
+            if (first < digits.Length / 2)
+            {
+                if (digits[first] == digits[last])
+                {
+                    DoesThisArrayAPalindrome(digits, first++, last--);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Generate digit array from the number.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <returns>Returns int[].</returns>
+        private static int[] IntToArray(int number)
+        {
+            int length = number.ToString().Length;
+            int[] digits = new int[length];
+
+            for (int j = length - 1; j >= 0; j--)
+            {
+                digits[j] = number % 10;
+                number /= 10;
+            }
+
+            return digits;
+        }
+    }
+}
