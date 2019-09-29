6a6c9f6cfa420921f019b60c76d043c1d6db0f2e
Olga Kripulevich <ochuvasheva@gmail.com>
2019-09-29 22:14:39 +0300

    added exceptions

--- BankSystem/BLL.Interface/Entities/AccountOwner.cs
+++ BankSystem/BLL.Interface/Entities/AccountOwner.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL.Interface.Exceptions;
+using System;
+using System.Text.RegularExpressions;
 
 namespace BLL.Interface.Entities
 {
@@ -8,9 +10,124 @@ namespace BLL.Interface.Entities
     /// <seealso cref="System.IEquatable{BLL.Interface.Entities.AccountOwner}" />
     public class AccountOwner : IEquatable<AccountOwner>
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; } // TODO: валидация имени (null, empty, длина строки)
-        public string Email { get; set; } // TODO: валидация с помощью Regex
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        /// <exception cref="FieldException">
+        /// Throws if value is null, empty or very long.
+        /// </exception>
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new FieldException($"{nameof(value)} can't be null.");
+                }
+
+                if (value == "")
+                {
+                    throw new FieldException($"{nameof(value)} can't be empty.");
+                }
+
+                if (value.Length > 20)
+                {
+                    throw new FieldException($"{nameof(value)} can't be longer than 20 symbols.");
+                }
+
+                firstName = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        /// <exception cref="FieldException">
+        /// Throws if value is null, empty or very long.
+        /// </exception>
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new FieldException($"{nameof(value)} can't be null.");
+                }
+
+                if (value == "")
+                {
+                    throw new FieldException($"{nameof(value)} can't be empty.");
+                }
+
+                if (value.Length > 20)
+                {
+                    throw new FieldException($"{nameof(value)} can't be longer than 20 symbols.");
+                }
+
+                lastName = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        /// <exception cref="FieldException">
+        /// Throws if value is null, empty or very long or isn't valid.
+        /// </exception>
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new FieldException($"{nameof(value)} can't be null.");
+                }
+
+                if (value == "")
+                {
+                    throw new FieldException($"{nameof(value)} can't be empty.");
+                }
+
+                if (value.Length > 30)
+                {
+                    throw new FieldException($"{nameof(value)} can't be longer than 30 symbols.");
+                }
+
+                if (isValid(value))
+                {
+                    email = value;
+                }
+                else
+                {
+                    throw new FieldException($"{nameof(value)} isn't valid.");
+                }
+            }
+        }
 
         /// <summary>
         /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
@@ -56,5 +173,11 @@ namespace BLL.Interface.Entities
             }
         }
 
+        private bool isValid(string email)
+        {
+            string pattern = "[.\\-_a-z0-9]+@([a-z0-9][\\-a-z0-9]+\\.)+[a-z]{2,6}";
+            Match isMatch = Regex.Match(email, pattern, RegexOptions.IgnoreCase);
+            return isMatch.Success;
+        }
     }
 }
---
+++ BankSystem/BLL.Interface/Exceptions/FieldException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BLL.Interface.Exceptions
+{
+    public class FieldException : Exception
+    {
+        const string validMessage = "Incorrect data entered in the field.";
+
+        public FieldException() : base(validMessage)
+        { }
+
+        public FieldException(string auxMessage) :
+            base(String.Format("{0} - {1}", auxMessage, validMessage))
+        { }
+    }
+}
