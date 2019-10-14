fd3bf5e5e88600604f26f09d4c49c57cb8dbea36
Olga Kripulevich <olga.kripulevich@gmail.com>
2019-10-14 14:42:54 +0300

    Update PrimeNumberGeneratorTests.cs

--- NET.Autumn.2019.Kripulevich.10/PrimeNumbersTests/PrimeNumberGeneratorTests.cs
+++ NET.Autumn.2019.Kripulevich.10/PrimeNumbersTests/PrimeNumberGeneratorTests.cs
@@ -21,12 +21,6 @@ namespace PrimeNumbersTests
             {
                 return PrimeNumberGenerator.GetPrimeNumbers(maxNumber).ToArray();
             }
-
-            [Test]
-            public void GetPrimeNumbers_Zero_ArgumentExeption()
-            {
-                Assert.Throws<ArgumentException>(() => PrimeNumberGenerator.GetPrimeNumbers(0));
-            }
         }
 
         public class DataClass
@@ -35,6 +29,7 @@ namespace PrimeNumbersTests
             {
                 get
                 {
+                    yield return new TestCaseData(0).Returns(new ArgumentException());
                     yield return new TestCaseData(2).Returns(new BigInteger[] { 2 });
                     yield return new TestCaseData(4).Returns(new BigInteger[] { 2, 3 });
                     yield return new TestCaseData(60).Returns(new BigInteger[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59 });
