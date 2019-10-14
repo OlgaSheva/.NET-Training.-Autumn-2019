6327831622ae814ef1c2b43267794911f86e9a95
Olga Kripulevich <olga.kripulevich@gmail.com>
2019-10-14 15:14:31 +0300

    Update ParenthesesTests.cs

--- NET.Autumn.2019.Kripulevich.10/CorrectParenthesesTests/ParenthesesTests.cs
+++ NET.Autumn.2019.Kripulevich.10/CorrectParenthesesTests/ParenthesesTests.cs
@@ -9,6 +9,7 @@ namespace CorrectParenthesesTests
         [TestCase("sdf(sdg{sdg}sdg)[]", ExpectedResult = true)]
         [TestCase("((({{)()}}}[][](({]", ExpectedResult = false)]
         [TestCase("{([])}{()[()]}", ExpectedResult = true)]
+        [TestCase("(8*3)+[(9-11)/{13}]", ExpectedResult = true)]
         public bool Correct(string @string)
         {
             return Parentheses.Correct(@string);
