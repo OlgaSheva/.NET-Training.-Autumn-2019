36b0e762b6dfa9f54954480ae2fefbcf7f3d8b71
Olga Kripulevich <olga.kripulevich@gmail.com>
2019-10-29 21:18:12 +0300

    added day 18 task 1 and 2

---
+++ NET.Autumn.2019.Kripulevich.18/Streams-master/StreamDemo/StreamsExtension.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StreamDemo
+{
+    // C# 6.0 in a Nutshell. Joseph Albahari, Ben Albahari. O'Reilly Media. 2015
+    // Chapter 15: Streams and I/O
+    // Chapter 6: Framework Fundamentals - Text Encodings and Unicode
+    // https://docs.microsoft.com/en-us/dotnet/api/system.text.encoding?view=netcore-3.0
+    // https://docs.microsoft.com/en-us/dotnet/api/system.io?view=netcore-3.0
+
+    public static class StreamsExtension
+    {
+        #region Public members
+
+        #region TODO: Implement by byte copy logic using class FileStream as a backing store stream .
+
+        public static int ByByteCopy(string sourcePath, string destinationPath)
+        {
+            InputValidation(sourcePath, destinationPath);
+
+            int count = 0;
+            using (FileStream fileStreamSource = new FileStream(sourcePath, FileMode.Open))
+            using (FileStream fileStreamDest = new FileStream(destinationPath, FileMode.Create))
+            {
+                int b;
+                while ((b = fileStreamSource.ReadByte()) > 0)
+                {
+                    fileStreamDest.WriteByte((byte)b);
+                }
+            }
+            return count;
+        }
+
+        #endregion
+
+        #region TODO: Implement by byte copy logic using class MemoryStream as a backing store stream.
+
+        public static int InMemoryByByteCopy(string sourcePath, string destinationPath)
+        {
+            InputValidation(sourcePath, destinationPath);
+
+            var UTF8 = new UTF8Encoding(true);
+            int count = 0;
+            string @string;
+            using (StreamReader reader = new StreamReader(sourcePath, UTF8))
+            {
+                @string = reader.ReadToEnd();
+            }
+            var byteBuffer = UTF8.GetBytes(@string);
+            MemoryStream memoryStream = new MemoryStream(byteBuffer);
+            var arrayBuffer = memoryStream.ToArray();
+            var charBuffer = UTF8.GetString(arrayBuffer);
+            using (StreamWriter streamWriter = new StreamWriter(destinationPath, false, UTF8))
+            {
+                foreach (var item in charBuffer)
+                {
+                    streamWriter.Write(item);
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        #endregion
+
+        #region TODO: Implement by block copy logic using FileStream buffer.
+        
+        public static int ByBlockCopy(string sourcePath, string destinationPath)
+        {
+            InputValidation(sourcePath, destinationPath);
+
+            int count = 0;
+            int bufferSize = 1024;
+            using (FileStream sourceFile = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))
+            using (FileStream destinationFile = new FileStream(destinationPath, FileMode.Create, FileAccess.Write))
+            {
+                byte[] buffer = new byte[bufferSize];
+                int length = 0;
+                while ((length = sourceFile.Read(buffer, 0, bufferSize)) > 0)
+                {
+                    destinationFile.Write(buffer, 0, length);
+                    count += length;
+                }
+            }
+            return count;
+        }
+
+        #endregion
+
+        #region TODO: Implement by block copy logic using MemoryStream.
+        
+        public static int InMemoryByBlockCopy(string sourcePath, string destinationPath)
+        {
+            InputValidation(sourcePath, destinationPath);
+
+            var UTF8 = new UTF8Encoding(true);
+            int bufferSize = 1024;
+            char[] charBuffer = new char[bufferSize];
+            int length = 0;
+            int count = 0;
+            using (StreamReader sourceFile = new StreamReader(sourcePath, UTF8))
+            using (StreamWriter destinationWriter = new StreamWriter(destinationPath, false, UTF8))
+            {
+                while ((length = sourceFile.ReadBlock(charBuffer, 0, bufferSize)) > 0)
+                {
+                    byte[] byteBuffer = new byte[length];
+                    byteBuffer = UTF8.GetBytes(charBuffer, 0, length);
+                    MemoryStream memoryStream = new MemoryStream(byteBuffer);
+                    byte[] newByteBuffer = memoryStream.ToArray();
+                    var newCharBuffer = new char[length]; 
+                    UTF8.GetChars(newByteBuffer, newCharBuffer);
+                    foreach (var c in newCharBuffer)
+                    {
+                        destinationWriter.Write(c);
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        #endregion
+
+        #region TODO: Implement by block copy logic using class-decorator BufferedStream.
+
+        public static int BufferedCopy(string sourcePath, string destinationPath)
+        {
+            InputValidation(sourcePath, destinationPath);
+
+            int bufferSize = 1024;
+            int length = 0;
+            byte[] buffer = new byte[bufferSize];
+            using (var sourceFile = new FileStream(sourcePath, FileMode.Open))
+            using (var destinationFile = new FileStream(destinationPath, FileMode.Create))
+            using (var sourceBufferFile = new BufferedStream(sourceFile, bufferSize))
+            using (var destinationBufferFile = new BufferedStream(destinationFile, bufferSize))
+            {
+                while ((length = sourceFile.Read(buffer, 0, bufferSize)) > 0)
+                {
+                    destinationFile.Write(buffer, 0, length);
+                }
+            }
+            return destinationPath.Length;
+        }
+
+        #endregion
+
+        #region TODO: Implement by line copy logic using FileStream and classes text-adapters StreamReader/StreamWriter
+
+        public static int ByLineCopy(string sourcePath, string destinationPath)
+        {
+            InputValidation(sourcePath, destinationPath);
+            var UTF8 = new UTF8Encoding(true);
+            int count = 0;
+            string buffer;
+            using (var sourceFile = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))
+            using (var destinationFile = new FileStream(destinationPath, FileMode.Create, FileAccess.Write))
+            using (var sourceReader = new StreamReader(sourceFile, UTF8))
+            using (var destinationWriter = new StreamWriter(destinationFile, UTF8))
+            {
+                while ((buffer = sourceReader.ReadLine()) != null)
+                {
+                    destinationWriter.WriteLine(buffer);
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Private members
+
+        #region TODO: Implement validation logic
+
+        private static void InputValidation(string sourcePath, string destinationPath)
+        {
+            if (sourcePath is null)
+            {
+                throw new ArgumentNullException(nameof(sourcePath));
+            }
+
+            if (destinationPath == null)
+            {
+                throw new ArgumentNullException(nameof(destinationPath));
+            }
+
+            if (!File.Exists(sourcePath))
+            {
+                throw new FileNotFoundException(
+                    $"File '{sourcePath}' not found. Parameter name: {nameof(sourcePath)}.");
+            }
+
+//            if (!File.Exists(destinationPath))
+//            {
+//                try
+//                {
+//                    File.Create(destinationPath);
+//                }
+//                catch
+//                {
+//                    throw new FileNotFoundException(
+//                        $"File '{destinationPath}' not found and can not be created. Parameter name: {nameof(destinationPath)}.");
+//                }
+//            }
+
+//            if (new FileInfo(destinationPath).IsReadOnly)
+//            {
+//                throw new FieldAccessException(
+//                    $"File '{destinationPath}' is readonly. Parameter name: {nameof(destinationPath)}.");
+//            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ NET.Autumn.2019.Kripulevich.18/Streams.2/Streams/StreamTask.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using OfficeOpenXml;
+
+namespace Streams
+{
+	public static class StreamTask
+	{
+		/// <summary>
+		/// Parses Resources\Planets.xlsx file and returns the planet data: 
+		///   Jupiter     69911.00
+		///   Saturn      58232.00
+		///   Uranus      25362.00
+		///    ...
+		/// See Resources\Planets.xlsx for details
+		/// </summary>
+		/// <param name="xlsxFileName">Source file name.</param>
+		/// <returns>Sequence of PlanetInfo</returns>
+		public static IEnumerable<PlanetInfo> ReadPlanetInfoFromXlsx(string xlsxFileName)
+		{
+			if (xlsxFileName == null)
+            {
+                throw new ArgumentNullException(nameof(xlsxFileName));
+            }
+
+            using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(xlsxFileName)))
+            {
+                var sheet1 = xlPackage.Workbook.Worksheets.First();
+                var totalRows = sheet1.Dimension.End.Row;
+                var totalColumns = sheet1.Dimension.End.Column;
+                
+                for (int rowNum = 2; rowNum <= totalRows; rowNum++)
+                {
+                    var row = sheet1.Cells[rowNum, 1, rowNum, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+                    row = row.Where(x => x != string.Empty);
+                    if (!double.TryParse(row.Last(), out double radious))
+                    {
+                        throw new ArgumentException("Invalid radious.", nameof(xlsxFileName));
+                    }
+
+                    yield return new PlanetInfo() { Name = row.First(), MeanRadius = radious };
+                }
+            }
+        }
+
+		/// <summary>
+		/// Calculates hash of stream using specified algorithm.
+		/// </summary>
+		/// <param name="stream">Source stream</param>
+		/// <param name="hashAlgorithmName">
+		///     Hash algorithm ("MD5","SHA1","SHA256" and other supported by .NET).
+		/// </param>
+		/// <returns></returns>
+		public static string CalculateHash(this Stream stream, string hashAlgorithmName)
+		{
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            if (hashAlgorithmName == null)
+            {
+                throw new ArgumentNullException(nameof(hashAlgorithmName));
+            }
+
+            if (string.IsNullOrWhiteSpace(hashAlgorithmName))
+            {
+                throw new ArgumentException("Hash algorithm name canbot be empty.", nameof(hashAlgorithmName));
+            }
+
+            var hashAlgorithm = HashAlgorithm.Create(hashAlgorithmName)
+                ?? throw new ArgumentException("Invalid algorithm.", nameof(hashAlgorithmName));
+            byte[] hash = hashAlgorithm.ComputeHash(stream);
+            return string.Join(
+                string.Empty,
+                hash.Select(x => x.ToString("X2")));
+        }
+
+        /// <summary>
+        /// Returns decompressed stream from file. 
+        /// </summary>
+        /// <param name="fileName">Source file.</param>
+        /// <param name="method">Method used for compression (none, deflate, gzip).</param>
+        /// <returns>output stream</returns>
+        public static Stream DecompressStream(string fileName, DecompressionMethods method)
+		{
+            if (fileName == null)
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException($"The {nameof(fileName)} cannot be empty.", nameof(fileName));
+            }
+
+            FileStream fileStream = new FileStream(fileName, FileMode.Open);
+
+            switch (method)
+            {
+                case DecompressionMethods.Deflate:
+                    return new DeflateStream(fileStream, CompressionMode.Decompress);
+                case DecompressionMethods.GZip:
+                    return new GZipStream(fileStream, CompressionMode.Decompress);
+                default:
+                    return fileStream;
+            }
+        }
+
+		/// <summary>
+		/// Reads file content encoded with non Unicode encoding
+		/// </summary>
+		/// <param name="fileName">Source file name</param>
+		/// <param name="encoding">Encoding name</param>
+		/// <returns>Unicoded file content</returns>
+		public static string ReadEncodedText(string fileName, string encoding)
+		{
+            if (fileName == null)
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            if (encoding == null)
+            {
+                throw new ArgumentNullException(nameof(encoding));
+            }
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException($"The {nameof(fileName)} cannot be empty.", nameof(fileName));
+            }
+
+            if (string.IsNullOrWhiteSpace(encoding))
+            {
+                throw new ArgumentException($"The {nameof(encoding)} cannot be empty.", nameof(encoding));
+            }
+
+            using (StreamReader reader = new StreamReader(fileName, Encoding.GetEncoding(encoding)))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+	}
+}
